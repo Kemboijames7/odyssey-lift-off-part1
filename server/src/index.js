8cbcde5e2bf2e8030dbbcdcfbada5064ade06905
James <jameskemich@gmail.com>
2025-01-08 23:59:38 +0300

    practiced  Apollo Explorer code

--- server/src/index.js
+++ server/src/index.js
@@ -12,14 +12,11 @@ const mocks = {
     Track: () => ({
       id: () => "track_01",
       title: () => "Astro Kitty, Space Explorer",
-      author: () => {
-        return {
-          name: "Grumpy Cat",
-          photo:
-            "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
-        };
-      },
-      thumbnail: () =>
+      author: {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/catstrophysicist_bqfh9n_j0amow.jpg",
+      },bnail: () =>
         "https://res.cloudinary.com/apollographql/image/upload/v1730818804/odyssey/lift-off-api/nebula_cat_djkt9r_nzifdj.jpg",
       length: () => 1210,
       modulesCount: () => 6,
